c4ae30defe160fde72eb07679c48cfea93ca1029
Gideon <125488605+GideonJulian@users.noreply.github.com>
2023-06-12 04:42:06 +0100

    new upadte

--- js/main.js
+++ js/main.js
@@ -1,47 +1,46 @@
-var previewContainer = document.querySelector('.product-prev')
-var  previewBox = previewContainer.querySelectorAll('.preview')
+var previewContainer = document.querySelector(".product-prev");
+var previewBox = previewContainer.querySelectorAll(".preview");
 
-
-    document.querySelectorAll('.product-container .aboutBtn').forEach(product =>{
-        product.onclick = () =>{
-            previewContainer.style.display = 'flex'
-            let name = product.getAttribute('data-name')
-            previewBox.forEach(preview =>{
-                var target = preview.getAttribute('data-target')
-                if(name == target){
-                    preview.classList.add("active")
-               }
-            })
+var AboutBtn = document
+  .querySelectorAll(".product-container .aboutBtn")
+  .forEach((product) => {
+    product.onclick = () => {
+      previewContainer.style.display = "flex";
+      let name = product.getAttribute("data-name");
+      previewBox.forEach((preview) => {
+        var target = preview.getAttribute("data-target");
+        if (name == target) {
+          preview.classList.add("active");
         }
-    })
+      });
+    };
+  });
 
+previewBox.forEach((close) => {
+  close.querySelector("#closePro").onclick = () => {
+    close.classList.remove("active");
+    console.log(1);
+    previewContainer.style.display = "none";
+  };
+});
 
-previewBox.forEach(close =>{
-    close.querySelector("#closePro").onclick = () =>{
-        close.classList.remove('active')
-        console.log(1) 
-        previewContainer.style.display = 'none'   
-    }
-})
-
-
-// FLITERING PRODUCTS 
+// FLITERING PRODUCTS
 var filteringBtn = document.querySelectorAll("#buttons .btn");
 
 var items = document.querySelectorAll(".product-container .card");
-window.addEventListener('DOMContentLoaded', ()=>{
-  
-})
+window.addEventListener("DOMContentLoaded", () => {});
 var filterFunction = (e) => {
   var activeBtn = document.querySelector(".active");
   activeBtn.classList.remove("active");
   e.target.classList.add("active");
-  items.forEach(card =>{
-    card.classList.add("hide")
-    card.style.margin = '20px'
-    if(card.dataset.name == e.target.dataset.name || e.target.dataset.name == 'all'){
-        card.classList.remove("hide")
-
+  items.forEach((card) => {
+    card.classList.add("hide");
+    card.style.margin = "20px";
+    if (
+      card.dataset.name == e.target.dataset.name ||
+      e.target.dataset.name == "all"
+    ) {
+      card.classList.remove("hide");
     }
     // var NotFound = document.querySelector('.no-found')
     // if(card.dataset.name != e.target.dataset.name){
@@ -50,10 +49,11 @@ var filterFunction = (e) => {
     //     NotFound.style.display = 'none'
 
     // }
-  })
+  });
 };
 
-filteringBtn.forEach(buttons => buttons.addEventListener("click", filterFunction));
-
+filteringBtn.forEach((buttons) =>
+  buttons.addEventListener("click", filterFunction)
+);
 
 // FILTERING SEARCHBOX
